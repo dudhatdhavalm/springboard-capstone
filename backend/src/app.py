dcecde479e41bc34c1e9c3b481ad18ce28868fea
dhaval <dhavaldudhat81@gmail.com>
2021-04-03 15:42:10 +0530

    Data Wrangling And model

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,36 @@
-from flask import Flask
+from flask import Flask, request
+import pickle
+import pandas as pd
+import os
+import sklearn
+from sklearn.linear_model import LogisticRegression
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello"
 
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    model_data_filename = "./data/prod/final_data.csv"
+    model_filename = "./data/prod/finalized_model.sav"
+    print(os.getcwd())
+    df = pd.read_csv(model_data_filename)
+    columns = []
+    for column in df.columns:
+        if column != "loan_status":
+            columns.append(column)
+    
+    param_df = df.iloc[[1000]]
+
+    params = param_df[columns].values
+    lr = pickle.load(open(model_filename, 'rb'))
+    print(type(lr))
+    # print(lr.predict(params))
+    return "Hello"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
